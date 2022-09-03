647303ecf2c48ec0e5742598cea1f014b6327eef
Mykyta Storozhenko <mst387@uky.edu>
2022-09-02 20:49:05 -0400

    Add pixel count mechanism

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
 const gridLength = document.getElementById("gridContainer").offsetWidth;
-const sidePixelNumber = document.getElementById("pixelSize").value;
+/*const sidePixelNumber = document.getElementById("pixelSize").value;*/
 const colorChoice = document.getElementById("userColorChoice").value; 
 const canvas = document.getElementById("gridContainer");
+
+document.getElementsByName("gridNumber")[0].addEventListener("change", canvasDraw);
+function canvasDraw(){
+    const sidePixelNumber = this.value;
+    const pixelSize = gridLength / sidePixelNumber; 
+    const totalPixelNumber = sidePixelNumber * sidePixelNumber;
+    for (let index = 0; index < totalPixelNumber; index++) {
+        const pixelDiv = document.createElement("div");
+        pixelDiv.style.width = pixelSize + "px";
+        pixelDiv.style.height = pixelSize + "px";
+        pixelDiv.style.backgroundColor = "black";
+        pixelDiv.style.display = "block";
+        pixelDiv.style.margin = 0 +"px";
+        canvas.appendChild(pixelDiv);
+    }
+}
 
 function populateCanvas(gridLength, sidePixelNumber){
     const pixelSize = gridLength / sidePixelNumber; 
@@ -13,12 +29,11 @@ function populateCanvas(gridLength, sidePixelNumber){
         pixelDiv.style.backgroundColor = "black";
         pixelDiv.style.display = "block";
         pixelDiv.style.margin = 0 +"px";
-        pixelDiv.style.borderWidth = 0 + "px";
         canvas.appendChild(pixelDiv);
     }
 }
 
-populateCanvas(gridLength, sidePixelNumber);
+
 
 // individual pixel size = gridlength/sidePixelNumber//
 // 
