e799a09e4e8845c6f3dd9efdbb77711c38195552
Mykyta Storozhenko <mst387@uky.edu>
2022-09-02 20:08:38 -0400

    Add JS script, issue is modding grid w/h

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
-function getUnitSize(){
-    const unitSize = prompt("Enter Pixel Size")
-    return unitSize;
+// document.getElementById("mydiv").offsetWidth //
+const gridLength = document.getElementById("gridContainer").offsetWidth;
+const sidePixelNumber = document.getElementById("pixelSize").value;
+const colorChoice = document.getElementById("userColorChoice").value; 
+const canvas = document.getElementById("gridContainer");
+
+function populateCanvas(gridLength, sidePixelNumber){
+    const pixelSize = gridLength / sidePixelNumber; 
+    const totalPixelNumber = sidePixelNumber * sidePixelNumber;
+    for (let index = 0; index < totalPixelNumber; index++) {
+        const pixelDiv = document.createElement("div");
+        pixelDiv.style.width = pixelSize + "px";
+        pixelDiv.style.height = pixelSize + "px";
+        pixelDiv.style.backgroundColor = "black";
+        pixelDiv.style.display = "block";
+        pixelDiv.style.margin = 0 +"px";
+        canvas.appendChild(pixelDiv);
+    }
 }
 
+populateCanvas(gridLength, sidePixelNumber);
 
-
-function createGrid(unitSize){
-
-}
+// individual pixel size = gridlength/sidePixelNumber//
+// 
